3cc9b52e66646d9ca9b6408e7015d38fb0d5233d
Juan <juanjulionn@gmail.com>
2025-01-16 18:25:21 +0100

    added groups

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
-def main(): 
+
+def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2   
-    player = Player(x,y)
-
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    while 1: 
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player.update(dt)
-        
-        screen.fill("#000000")
-        #print("Starting asteroids!")
-        #print(f"Screen width: {SCREEN_WIDTH}")
-        #print(f"Screen height: {SCREEN_HEIGHT}")
-        
 
+        for obj in updatable:
+            obj.update(dt)
 
-        player.draw(screen)
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -3,6 +3,7 @@ import pygame
 from constants import *
 
 class Player(CircleShape): 
+    containers = None
     def __init__(self, x, y):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
@@ -27,6 +28,15 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt): 
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
